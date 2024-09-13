3c3c4c64dca8a816f3c4719a5ba8998f6af3b6df
avikeseal <avikeseal@gmail.com>
2024-09-13 17:18:33 -0400

    declared wavelength range between 1 nm to 3 micrometers

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ c = 3.0e8
 k_B = 1.38e-23
 #Temperature in kelvin K representing approximate surface temp:
 T = 5000
+
+#Let's define the wavelength range:
+#We'll be defining the range of wavelength over which we'll calculate the spectral radiance
+#Starts from 1 nanometer (1e-9 meters) to 3 micrometers (3e-6 meters)
+#500 points between 1 nm to 3 micrometers
+wavelengths = np.linspace(1e-9, 3e-6, 500)
