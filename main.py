a021fdfaefff6d6a734e89a1b34d110d37512902
avikeseal <avikeseal@gmail.com>
2024-09-13 17:12:17 -0400

    defined physical constants

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+#Let's define some physical constants we'll be using for
+#Planck's law and Rayleigh-Jeans Law
+
+#Planck's constant in joules*second
+h = 6.626e-34
+#speed of light in meters per second
+c = 3.0e8
+#Boltzmann's constant in Joules per second
+k_B = 1.38e-23
+#Temperature in kelvin K representing approximate surface temp:
+T = 5000
